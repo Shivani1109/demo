0ad4f72f673a6ad8bdce845324a2c12f246ff425
Shivani1109 <71625443+Shivani1109@users.noreply.github.com>
2021-10-08 11:47:43 +0530

    Add files via upload
    test file

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test
+{
+public static void main(String args[])
+{
+Student s1=new Student(11,"Usha");
+Student s2=new Student(12,"Asha");
+
+System.out.println(s1.rollno);
+System.out.println(s1.name);
+System.out.println(s2.rollno);
+System.out.println(s2.name);
+
+}
+}
